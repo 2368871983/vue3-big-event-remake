1aecb579d39b843d49337073e49c6c846c22c577
2368871983 <2368871983@qq.com>
2024-12-28 23:33:29 +0800

    初始化提交

--- eslint.config.js
+++ eslint.config.js
@@ -3,25 +3,29 @@ import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
-
+  {
+    // 全局配置部分
+    rules: {
+      // 找到对应规则，注释掉或者修改其值
+      "vue/multi-word-component-names": ["error", {
+        "ignores": ['index']
+      }] // 将规则设为'off'禁用它
+    }
+  },
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
     rules: {
       "no-undef": "error" // 启用未定义变量检查
     },
-    "vue/no-setup-props-destructure": ['off'],
+
   },
 
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-  {
-    "vue/multi-word-component-names": ["error", {
-      "ignores": ['index']
-    }]
-  },
+  
   
 
   js.configs.recommended,
--- src/views/index.vue
+++ src/views/index.vue
@@ -5,6 +5,7 @@
 <script setup>
 
 
+
 </script>
 
 <style lang="scss" scoped></style>
